d95e4633e59f92d60380b1aebf98ccb8c5b5f59f
nayakunin <nayakunin@outlook.com>
2020-02-23 13:22:52 +0300

    added styles for card component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './index.css';
 import './images/image-03.jpg';
 import './images/not-found_v1.png';
+
+const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+let maxSymbols;
+
+if (vw >= 1440) {
+  maxSymbols = 150;
+} else if (vw >= 1024) {
+  maxSymbols = 90;
+} else if (vw >= 768) {
+  maxSymbols = 80;
+}
+
+function truncate(element, maxLength) {
+  let truncated = element.innerText;
+  if (truncated.length > maxLength) {
+    truncated = truncated.substr(0, maxLength) + '...';
+  }
+  return truncated;
+}
+
+const cards = document.getElementsByClassName('card');
+cards.forEach((card) => {
+  const description = card.getElementsByClassName('card__description')[0];
+  description.innerText = truncate(description, maxSymbols);
+})
